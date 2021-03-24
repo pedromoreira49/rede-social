665b4b00cb2c53831baa3436eadd3d08d9bdafaa
pedromoreira <jpedrostonemoreira@gmail.com>
2021-03-24 19:59:33 -0300

    Realizado o front-end da página de login.

--- ClassesMVC/Views/pages/login.php
+++ ClassesMVC/Views/pages/login.php
@@ -7,6 +7,23 @@
 	<link href="<?php echo INCLUDE_PATH_STATIC ?>Styles/styles.css" rel="stylesheet">
 </head>
 <body>
-	<h2>Olá mundo</h2>
+	<div class="sidebar"></div>
+
+	<div class="form-container-login">
+		<div class="logo">
+			<img src="<?php echo INCLUDE_PATH_STATIC ?>images/logo-redesocial.png">
+			<p>Junte-se a outras pessoas e venha fazer parte deste mar de conhecimentos.</p>
+		</div><!--LOGO-->
+
+		<div class="form-login">
+			<form>
+				<input type="text" name="login">
+				<input type="password" name="senha">
+				<input type="submit" name="acao" value="Logar!">
+			</form>
+			<p><a href="<?php echo INCLUDE_PATH ?>registrar">Criar Conta</a></p>
+		</div><!--FORM-LOGIN-->
+
+	</div><!--FORM-CONTAINER-LOGIN-->
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	require('vendor/autoload.php');
 
 	define('INCLUDE_PATH_STATIC', 'http://localhost/redeSocial/ClassesMVC/Views/pages/');
+	define('INCLUDE_PATH', 'http://localhost/redeSocial/');
 
 	$app = new ClassesMVC\Application();
 
