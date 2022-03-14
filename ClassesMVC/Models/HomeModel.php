9c4e509f17a464b10a74237c3a5cd296cc962101
pedro moreira <jpedrostonemoreira@gmail.com>
2022-03-14 17:47:54 -0300

    Implementado função de chamada de imagens de fora da plataforma

--- ClassesMVC/Models/HomeModel.php
+++ ClassesMVC/Models/HomeModel.php
@@ -7,6 +7,13 @@
 		public static function postFeed($post){
 			$pdo = \ClassesMVC\Mysql::connect();
 			$post = strip_tags($post);
+
+			if(preg_match('/\[imagem/', $post)){
+				$post = preg_replace('/(.*?)\[imagem=(.*?)\]/', '<p>$1</p><img src="$2" />', $post);
+			}else{
+				$post = '<p>'.$post.'</p>';
+			}
+
 			$postFeed = $pdo->prepare("INSERT INTO `posts` (usuario_id, post, date) VALUES (?, ?, ?);");
 			$postFeed->execute(array($_SESSION['id'],$post,date('Y-m-d H:i:s',time())));
 
